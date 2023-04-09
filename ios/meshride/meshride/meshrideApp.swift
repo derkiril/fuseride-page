047fe4517a54d5fbd0089118b5d1ab4d5999028e
Anna Klara Cominelli <anna.cominelli@esentri.com>
2023-04-09 21:42:13 +0200

    ios app structure

---
+++ ios/meshride/meshride/meshrideApp.swift
@@ -0,0 +1,30 @@
+//
+//  meshrideApp.swift
+//  meshride
+//
+//  Created by Anna Klara on 09.04.23.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+@main
+struct meshrideApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
